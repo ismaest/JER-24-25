8c6d461b4fc8fcd701014d52ef31a4ff7e4fd9f3
ReibaxL <115174097+ReibaxL@users.noreply.github.com>
2024-12-12 12:28:55 +0100

    Location Updated

---
+++ LabRatsServer/src/main/java/com/example/demo/LabRatsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabRatsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabRatsServerApplication.class, args);
+	}
+
+}
