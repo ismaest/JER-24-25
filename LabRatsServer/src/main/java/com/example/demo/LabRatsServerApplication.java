4ff58fb7b50e464275cd61440f966fa53c752de6
ReibaxL <115174097+ReibaxL@users.noreply.github.com>
2024-12-14 18:19:39 +0100

    User class updated with more and better methods

--- LabRatsServer/src/main/java/com/example/demo/LabRatsServerApplication.java
+++ LabRatsServer/src/main/java/com/example/demo/LabRatsServerApplication.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LabRatsServerApplication {
-
+	
+	@Bean
+	public User user() {
+		return new User("", "");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LabRatsServerApplication.class, args);
 	}
--- LabRatsServer/src/main/java/com/example/demo/UserController.java
+++ LabRatsServer/src/main/java/com/example/demo/UserController.java
@@ -2,9 +2,14 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.lang.*;
 
+@Controller
 public class UserController {
 	
 	//VARIABLES
@@ -17,20 +22,15 @@ public class UserController {
 	
 	//METHODS
 	
+	
 	//Method called when trying to access an existing user. The method requires a password and a name to find said user
 	@GetMapping
 	public User GetUser(String name, String password) {
 		
-		String encodedPass = encoder.encode(password);
-		
-		boolean success = encoder.matches(password,  encodedPass);
-		
-		
 		User userToFind = new User(name, password);
 		
-		
 		if(user.UserExists(userToFind)){
-			return userToFind;
+			return user.GetUser(userToFind); //if the user as found, the method return the user that was being asked for
 		}
 		
 		return new User("", ""); //Returns an empty user, meaning it didn't find a user with the specified name and password
@@ -45,10 +45,36 @@ public class UserController {
 		User newUser = new User(name, password);
 		
 		if(!user.UserExists(newUser)) {
-			return newUser; //Returns the created user because it didn't exist an exact one
+			user.AddUser(newUser); //The user is added to the userArray
+			return user.GetUser(newUser); //Returns the created user because
 		}
 		
-		return new User("", ""); //Returns an empty user, meaning there was already a username with the same name
+		return new User("", ""); //Returns an empty user, meaning there was already a user name with the same name
+		
+	}
+	
+	
+	//Method called when trying to update a player with a new user name and password
+	@PutMapping
+	public User UpdateUser(String name, String password) {
+		
+		User newUser = new User(name, password);
+		
+		if(user.UserExists(newUser)) {
+			user.UpdateUser(user.GetUser(newUser), newUser); //It takes the old user and updates it with the new one
+			return user.GetUser(newUser); //It returns the newly updated user
+		}
+		
+		return new User("", ""); //If there hasn't been an update, the method return an empty User
+		
+	}
+	
+	
+	//Method called when trying to delete a user
+	@DeleteMapping
+	public void DeleteUser(String name, String password) {
+		
+		user.DeleteUser(new User(name, password)); //Calls to the deletion method with the specified user to delete
 		
 	}
 	
